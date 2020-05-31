502d748fb952db599be202bdc8ac4a29a3d5dec4
Tatsunomiya <Black1940G>
2020-05-31 15:58:49 +0900

    test

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let skView = self.view as! SKView
+        
+        
+        skView.showsFPS = true
+        
+        skView.showsNodeCount = true
+        
+        let scene = GameScene(size:skView.frame.size)
+        
+        skView.presentScene(scene)
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        get{
+            return true
+        }
     }
 
 
